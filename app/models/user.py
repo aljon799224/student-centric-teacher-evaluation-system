42f9d89108aafa8eb0d58023eb97850da41edf4c
Aljon Mendiola <aljonmendiola799224@yahoo.com>
2024-12-10 17:32:54 +0800

    update user model

--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,8 @@
 """User model."""
 
-from sqlalchemy import Column, String, Integer, Boolean
+from datetime import datetime
+
+from sqlalchemy import Column, String, Integer, Boolean, DateTime
 
 from app.db.base_class import Base
 
@@ -16,3 +18,7 @@ class User(Base):
     last_name = Column(String, nullable=True)
     disabled = Column(Boolean, nullable=True)
     hashed_password = Column(String, nullable=True)
+    role = Column(String, nullable=True)
+    temp_pwd = Column(Boolean, default=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
