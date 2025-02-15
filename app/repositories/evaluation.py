0d7ea52c1bbd0b0c2e4e40e302cae47d2cc6fa1a
Aljon Mendiola <aljon_mendiola799224@yahoo.com>
2025-02-15 14:27:50 +0800

    Add filtering by teacher id in evaluation

--- app/repositories/evaluation.py
+++ app/repositories/evaluation.py
@@ -1,5 +1,9 @@
 """Evaluation Repository."""
 
+from typing import List, cast
+
+from sqlalchemy.orm import Session
+
 from app.models import Evaluation
 from app.repositories.base import BaseRepository
 from app.schemas import EvaluationUpdate, EvaluationIn
@@ -8,4 +12,13 @@ from app.schemas import EvaluationUpdate, EvaluationIn
 class EvaluationRepository(BaseRepository[Evaluation, EvaluationIn, EvaluationUpdate]):
     """Evaluation Repository Class."""
 
-    pass
+    @staticmethod
+    def get_all_by_teacher_id(db: Session, *, teacher_id: int) -> List[Evaluation]:
+        """Get by teacher_id."""
+        response = cast(
+            List[Evaluation],
+            db.query(Evaluation).filter(Evaluation.teacher_id == teacher_id).all(),
+        )
+        print(response)
+        print(response)
+        return response
